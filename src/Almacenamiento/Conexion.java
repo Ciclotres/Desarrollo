67f6a8e224c50f5a7963640008ac68eaf5610c2e
Oliver herrer <ohmy.0517@gmail.com>
2022-08-16 14:21:33 -0500

    Se inicializo la nueva rama basededatos en el proyecto desarrollo, generando el archivo Conexion.java

---
+++ src/Almacenamiento/Conexion.java
@@ -0,0 +1,17 @@
+package Almacenamiento;
+
+public class Conexion {
+
+    /*Connection conn = null; //Objeto para la conexion
+    Statement stmt = null;//Objeto para ejecutar consulta
+    ResultSet rs = null;//Objeto para recuperar resultados de consulta
+   */
+    public static void main(String[] args) {
+        System.out.println("Conexion a la base de datos");
+
+    }
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("PRIMER ENLACE CON EL GRUPO");
+        System.out.println(" hola mundo ");
+    }
+}
