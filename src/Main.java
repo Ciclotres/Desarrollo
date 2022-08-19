9d6a7c163bb9659b63246bb558a44fc8aa2e2f05
unknown <Jose.Rodriguez-ra@mail.escuelaing.edu.co>
2022-08-19 15:25:55 -0500

    agregar un merge front-end1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("PRIMER ENLACE CON EL GRUPO");
         System.out.println(" hola mundo ");
+        system.out.println("merge del front-end1");
     }
 }
