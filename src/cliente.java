077eabd6c9269c8f45f1f9cb7d84cb6ee01afdce
andresvallejo <gavallejo83@gmail.com>
2022-08-19 13:08:28 -0500

    Version 1.1 Merge

--- src/cliente.java
+++ src/cliente.java
@@ -1,7 +1,74 @@
 public class cliente {
-    public static void main(String[] args) {
-        System.out.print("Espacio para clientes");
-        System.out.print(8+4);
+
+    private String numero;
+    private String tipo;
+    private double saldoActual=0;
+    private double saldoMinimo=0;
+    private double valorInicial=0;
+
+    public cliente(String numero, String tipo, double valorInicial) {
+        this.numero = numero;
+        if(tipo.equalsIgnoreCase("ahorro") || tipo.equalsIgnoreCase("credito"))
+        {   this.tipo = tipo;
+            this.valorInicial = valorInicial;
+            this.saldoActual=valorInicial;
+            this.saldoMinimo= valorInicial*0.1;}
+
     }
 
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSaldoActual() {
+        return saldoActual;
+    }
+
+    public void setSaldoActual(double saldoActual) {
+        this.saldoActual = saldoActual;
+    }
+
+    public double getSaldoMinimo() {
+        return saldoMinimo;
+    }
+
+    public void setSaldoMinimo(double saldoMinimo) {
+        this.saldoMinimo = saldoMinimo;
+    }
+
+    public double getValorInicial() {
+        return valorInicial;
+    }
+
+    public void setValorInicial(double valorInicial) {
+        this.valorInicial = valorInicial;
+    }
+
+    public void consignar(double monto)
+    {
+        this.saldoActual+=monto;
+    }
+    public void retirar(double monto){
+        if((this.saldoActual-monto)<this.saldoMinimo){
+            System.out.println("Saldo Insuficiente");
+            System.out.println("El valor que puede retirar es:" + (this.saldoActual-this.saldoMinimo));
+
+        }
+        else{
+            System.out.println("El valor retirado es: "+monto);
+            this.saldoActual-=monto;
+        }
+    }
 }
